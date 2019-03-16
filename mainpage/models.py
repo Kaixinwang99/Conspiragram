37629a56e07380a204b26b2de03c48259f8957d1
Kaixinwang99 <2358925w@student.gla.ac.uk>
2019-03-16 17:02:59 +0000

    change models

--- mainpage/models.py
+++ mainpage/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     User = models.OneToOneField(User)
     Website = models.URLField(blank=True)
-    Display = BooleanField(default = True)
     def __str__(self):
         return self.User.UserID
     
@@ -13,7 +12,7 @@ class User(models.Model):
     UserID = models.EmailField(max_length=254, unique = True)
     Username = models.CharField(max_length=128)
     Rank = models.CharField(max_length=128)
-    RankScore = meodels.FloatField()
+    RankScore = models.FloatField()
     def __str__(self):
         return self.UserID
 
@@ -24,13 +23,12 @@ class Picture(models.Model):
     Date = models.DateField(auto_now_add=True)
     Location = models.CharField(max_length=128)
     PictureID = models.IntegerField (unique = True)
-    Comments = models.ForeignKey(Comments)
     def __str__(self):
         return self.PictureID
 
 class Comments(models.Model):
     Picture = models.ForeignKey(Picture)
-    Text = model.CharField(max_lenth = 128)
+    Text = models.CharField(max_length=128)
     def __str__(self):
         return self.Text
     
