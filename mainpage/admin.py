cd031a589a41ce9ee942e04b1068272a44ce3fab
Kaixinwang99 <2358925w@student.gla.ac.uk>
2019-03-20 15:53:44 +0000

    add admin

--- mainpage/admin.py
+++ mainpage/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from rango.models import Picture,Comments
+from rango.models import UserProfile
 
-# Register your models here.
+#I just did a sample code you can change and fix it as you wish
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ("picture", "description", "TruthVotes", "FalseVotes", "Location")
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('Text')
+
+class UserProfileAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('User.username')}
+    
+admin.site.register(Picture, PictureAdmin)
+admin.site.register(Comments, CommentsAdmin)
+admin.site.register(UserProfile,UserProfileAdmin)
