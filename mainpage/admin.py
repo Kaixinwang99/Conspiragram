91e174827128a337dd5b491ab3963739b3b3ed7b
Kaixinwang99 <2358925w@student.gla.ac.uk>
2019-03-20 16:39:18 +0000

    bug fixed

--- mainpage/admin.py
+++ mainpage/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 from mainpage.models import Picture,Comments
 from mainpage.models import UserProfile
-'''
+
 #I just did a sample code you can change and fix it as you wish
 class PictureAdmin(admin.ModelAdmin):
-    list_display = ("picture", "description", "TruthVotes", "FalseVotes", "Location")
+    list_display = ("avatar", "description", "TruthVotes", "FalseVotes", "Location")
 
 class CommentsAdmin(admin.ModelAdmin):
-    list_display = ('Text')
+    list_display = ('Text',)
 
 class UserProfileAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('User.username')}
+    prepopulated_fields = {'slug':('User',)}
     
 admin.site.register(Picture, PictureAdmin)
 admin.site.register(Comments, CommentsAdmin)
 admin.site.register(UserProfile,UserProfileAdmin)
-'''
+
