7a04e640f7a491df3b2b6efe6572b8d5de8ce3cd
Hugo Fernández <workstudy@glaroam2-188-136.wireless.gla.ac.uk>
2019-03-21 15:06:08 +0000

    views.py update

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
 from datetime import datetime
+
 from mainpage.models import Picture,Comments,UserProfile
-#from mainpage.forms import
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponseRedirect, HttpResponse
+from mainpage.forms import *
+from mainpage.models import User
+
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 
 # A helper method
@@ -32,12 +36,41 @@ def visitor_cookie_handler(request):
     request.session['visits'] = visits
     
 def index(request):
-        context_dict={}
+        # context_dict={}
         request.session.set_test_cookie()
         visitor_cookie_handler(request)
         context_dict['visits'] = request.session['visits']
-        request = render(request, 'mainpage/index.html', context_dict)
-        return request
+        # request = render(request, 'mainpage/index.html', context_dict)
+        
+        if request.user.is_authenticated():
+            return redirect("mainpage")
+
+        if request.method == "POST":
+            sign_up_form = SignUpForm(request.POST)
+            login_form = LogInForm(request.POST)
+
+            if sign_up_form.is_valid() and request.POST.get("submit") == "Sign Up":
+                sign_up_form.save()
+                user_email = sign_up_form.cleaned_data.get("email")
+                user_password = sign_up_form.cleaned_data.get("password1")
+
+            elif request.POST.get("submit") == "Login":
+                user_email = request.POST.get("email")
+                user_password = request.POST.get("password")
+
+            user = authenticate(email=user_email, password=user_password)
+
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    return redirect("mainpage")
+
+        else:
+            sign_up_form = SignUpForm()
+            login_form = LogInForm()
+
+        return render(request, "mainpage/index.html", {"sign_up_form": sign_up_form,
+                                                       "login_form": login_form})
 	
 def about(request):
         context_dict={}
@@ -62,8 +95,9 @@ def user_profile(request, user_profile_slug):
                 request = render(request, 'mainpage/user_profile.html', context_dict)
                 return request
 
-#@login_required
+@login_required(login_url="index")
 def mainpage(request):
+    feed = Picture.objects.order_by("-date_published")
 	if request.user.is_authenticated():
 		request = render(request, 'mainpage/mainpage.html', context={})
 		return request
@@ -71,6 +105,37 @@ def mainpage(request):
 		context_dict={}
 		request = render(request, 'mainpage/index.html', context_dict)
 		return request
+
+    if request.method == "POST":
+        if request.POST.get("submit") == "Sign Out":
+            logout(request)
+            return redirect("index")
+
+        elif request.POST.get("submit") == "Upload":
+            upload_form = ImageUploadForm(request.POST, request.FILES)
+            if upload_form.is_valid():
+                new_picture = Picture(author=request.user,
+                                      picture=request.FILES["picture"],
+                                      description=request.POST["description"])
+                new_picture.save()
+                return redirect("mainpage")
+
+        elif request.POST.get("submit") == "Comment":
+            new_comment = Comment(author=request.user,
+                                  picture=Picture.objects.get(slug=request.POST["picture"]),
+                                  comment=request.POST["comment"])
+            new_comment.save()
+            return redirect("mainpage")
+
+    else:
+        upload_form = ImageUploadForm()
+        comment_form = CommentSubmissionForm()
+
+    return render(request, "mainpage/mainpage.html", {"feed": feed,
+                                                      "upload_form": upload_form,
+                                                      "comment_form": comment_form})
+
+
 
 
 	
