85b0c80fbd5a82c78c7c2ef3668fc0ef261e2b83
Hugo Fernández <workstudy@glaroam2-188-136.wireless.gla.ac.uk>
2019-03-21 14:50:05 +0000

    update ratings.js

--- static/js/ratings.js
+++ static/js/ratings.js
@@ -1,43 +1,43 @@
 function GetRatings(index) {
 
-    var health = document.getElementById("health_rating " + index.toString());
-    var healthSpans = document.getElementById( 'health_rating '  + index.toString()).getElementsByTagName( 'span' );
+    var truth = document.getElementById("truth_rating " + index.toString());
+    var truthSpans = document.getElementById( 'truth_rating '  + index.toString()).getElementsByTagName( 'span' );
 
-    var style = document.getElementById("style_rating " + index.toString());
-    var styleSpans = document.getElementById( 'style_rating ' + index.toString() ).getElementsByTagName( 'span' );
+    var fake = document.getElementById("fake_rating " + index.toString());
+    var fakeSpans = document.getElementById( 'fake_rating ' + index.toString() ).getElementsByTagName( 'span' );
 
-    var cooking = document.getElementById("cooking_rating " + index.toString());
-    var cookingSpans = document.getElementById( 'cooking_rating ' + index.toString() ).getElementsByTagName( 'span' );
+    var verified = document.getElementById("verified_rating " + index.toString());
+    var verifiedSpans = document.getElementById( 'verified_rating ' + index.toString() ).getElementsByTagName( 'span' );
 
     var empty_star = "fa fa-star";
-    var health_star_full = "fa fa-star checked_purple";
-    var style_star_full = "fa fa-star checked_red";
-    var cooking_star_full = "fa fa-star checked_blue";
+    var truth_star_full = "fa fa-star checked_green";
+    var fake_star_full = "fa fa-star checked_red";
+    var verified_star_full = "fa fa-star checked_blue";
 
-    SetPictureRatingsStarClass(healthSpans, health_star_full, empty_star, health);
-    SetPictureRatingsStarClass(styleSpans, style_star_full, empty_star, style);
-    SetPictureRatingsStarClass(cookingSpans, cooking_star_full, empty_star, cooking);
+    SetPictureRatingsStarClass(truthSpans, truth_star_full, empty_star, truth);
+    SetPictureRatingsStarClass(fakeSpans, fake_star_full, empty_star, style);
+    SetPictureRatingsStarClass(verifiedSpans, verified_star_full, empty_star, verified);
 
 }
 
 function GetUserRatings(index) {
-    var health = document.getElementById("user_health_rating " + index.toString());
-    var healthSpans = document.getElementById( 'user_health_rating '  + index.toString()).getElementsByTagName( 'span' );
+    var truth = document.getElementById("user_truth_rating " + index.toString());
+    var truthSpans = document.getElementById( 'user_truth_rating '  + index.toString()).getElementsByTagName( 'span' );
 
-    var style = document.getElementById("user_style_rating " + index.toString());
-    var styleSpans = document.getElementById( 'user_style_rating ' + index.toString() ).getElementsByTagName( 'span' );
+    var fake = document.getElementById("user_fake_rating " + index.toString());
+    var fakeSpans = document.getElementById( 'user_fake_rating ' + index.toString() ).getElementsByTagName( 'span' );
 
-    var cooking = document.getElementById("user_cooking_rating " + index.toString());
-    var cookingSpans = document.getElementById( 'user_cooking_rating ' + index.toString() ).getElementsByTagName( 'span' );
+    var verified = document.getElementById("user_verified_rating " + index.toString());
+    var verifiedSpans = document.getElementById( 'user_verified_rating ' + index.toString() ).getElementsByTagName( 'span' );
 
     var empty_star = "fa fa-star inside";
-    var health_star_full = "fa fa-star inside checked_purple";
-    var style_star_full = "fa fa-star inside checked_red";
-    var cooking_star_full = "fa fa-star inside checked_blue";
+    var truth_star_full = "fa fa-star inside checked_green";
+    var fake_star_full = "fa fa-star inside checked_red";
+    var verified_star_full = "fa fa-star inside checked_blue";
 
-    SetPictureRatingsStarClass(healthSpans, health_star_full, empty_star, health);
-    SetPictureRatingsStarClass(styleSpans, style_star_full, empty_star, style);
-    SetPictureRatingsStarClass(cookingSpans, cooking_star_full, empty_star, cooking);
+    SetPictureRatingsStarClass(truthSpans, truth_star_full, empty_star, truth);
+    SetPictureRatingsStarClass(fakeSpans, fake_star_full, empty_star, fake);
+    SetPictureRatingsStarClass(verifiedSpans, verified_star_full, empty_star, verified);
 }
 
 function SetPictureRatingsStarClass(spanList, checkedClass, uncheckedClass, rating_type) {
