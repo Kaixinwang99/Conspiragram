5653c212a70aa91fe8ddf44bd0632355b48eaeb3
Markus Zijlstra <2400899z@student.gla.ac.uk>
2019-03-22 14:02:02 +0000

    fixed pop script

--- populate_conspiragram.py
+++ populate_conspiragram.py
@@ -8,31 +8,31 @@ from mainpage.models import User, Picture, Comment, Rating
 def populate():
 	users = [
 		{"ID": "DDD@example.com",
-                "avatar": "profile1.jpg",
+                "avatar": "user_pictures/profile1.jpg",
 		"fname": "Dave",
                 "lname": "Strider",
 		"date": "2015-02-03",
 		"isstaff": "False"},
 		{"ID": "Shauna239@example.com",
-                "avatar": "profile2.jpg",
+                "avatar": "user_pictures/profile2.jpg",
 		"fname": "Shauna",
                 "lname": "Shining",
 		"date": "2015-12-23",
 		"isstaff": "False"},
 		{"ID": "theworst@example.com",
-                "avatar": "profile3.jpg",
+                "avatar": "user_pictures/profile3.jpg",
 		"fname": "The",
                 "lname": "Worst",
 		"date": "2016-06-15",
 		"isstaff": "False"},
 		{"ID": "theworst2@example.com",
-                "avatar": "profile3.jpg",
+                "avatar": "user_pictures/profile3.jpg",
 		"fname": "Not",
 		"lname": "AnAlt",
                 "date": "2016-06-15",
 		"isstaff": "False"},
 		{"ID": "prolificposter@example.com",
-                "avatar": "profile4.jpg",
+                "avatar": "user_pictures/profile4.jpg",
 		"fname": "Prolific",
                 "lname": "Poster",
 		"date": "2014-05-28",
@@ -40,72 +40,74 @@ def populate():
 	]
 	pictures = [
 		{"author": "Shauna239@example.com",
-		"picture": "ufo3.jpg",
+		"picture": "user_pictures/ufo3.jpg",
 		"date": "2018-06-20",
 		"description": "UFO spotted near Edinburgh",
-                "truth_rating": 30,
-                "false_rating": 35,
-                "verified_rating": "False"},
+                "truth_rating": 2,
+                "false_rating": 3,
+                "verified_rating": 2},
 		{"author": "prolificposter@example.com",
-		"picture": "bigfoot.jpeg",
+		"picture": "user_pictures/bigfoot.jpeg",
 		"date": "2016-02-01",
                 "description": "Original Bigfoot Photo",
 		"location": "California",
-                "truth_rating": 1079,
-                "false_rating": 885,
-                "verified_rating": "True"},
+                "truth_rating": 4,
+                "false_rating": 3,
+                "verified_rating": 25},
 		{"author": "theworst@example.com",
-                "picture": "ufo1.jpg",
+                "picture": "user_pictures/ufo1.jpg",
 		"date": "2019-03-13",
 		"description": "REAL UFO OUTSIDE MY HOUSE",
                 "truth_rating": 2,
-                "false_rating": 380,
-                "verified_rating": "False"},
-		{"author": "prolificposter@example.com",
-		"picture": "lochness.jpg",
+                "false_rating": 4,
+                "verified_rating": 0},
+		{"author": "DDD@example.com",
+		"picture": "user_pictures/lochness.jpg",
 		"date": "2016-08-01",
 		"description": "Loch Ness Monster",
-                "truth_rating": 1258,
-                "false_rating": 506,
-                "verified_rating": "True"}
+                "truth_rating": 4,
+                "false_rating": 1,
+                "verified_rating": 40}
 	]
 	comments = [
 		{"author": "theworst@example.com",
-		"picture": 0,
+		"picauthor": "Shauna239@example.com",
 		"comment": "lol fake",
 		"date": "2019-03-15"},
 		{"author": "theworst@example.com",
-		"picture": 0,
-		"comment": "lol fake",
+		"picauthor": "DDD@example.com",
+		"comment": "obv not real",
 		"date": "2019-03-15"},
 		{"author": "theworst@example.com",
-		"picture": 0,
-		"comment": "lol fake",
+		"picauthor": "prolificposter@example.com",
+		"comment": "lollllll fakeeeee",
 		"date": "2019-03-15"},
-		{"author": "theworst2@example.com,
-		"picture": 0,
+		{"author": "theworst2@example.com",
+		"picauthor": "theworst@example.com",
 		"comment": "best pic on here keep up the good work",
 		"date": "2019-03-15"},
 		{"author": "prolificposter@example.com",
-		"picture": 0,
+		"picauthor": "Shauna239@example.com",
 		"comment": "could be real but that's really out of focus!",
 		"date": "2019-03-16"},
-		{"author": "DDD@example.com,
-		"picture": 0,
-		"comment": "ah the original conspiracy!",
+		{"author": "DDD@example.com",
+		"picauthor": "DDD@example.com",
+		"comment": "the original conspiracy!",
 		"date": "2019-03-16"}
 	]
 	for u in users:
+		print("hi")
 		add_user(u["ID"], u["fname"], u["lname"], u["date"], u["isstaff"])
 	for p in pictures:
+		print("test")
 		add_picture(p["author"], p["picture"], p["description"], p["date"])
-		add_rating(p["author"], p["picture"], p["truth_rating"], p["false_rating"], p["verified_rating"]
+		add_rating(p["author"], p["truth_rating"], p["false_rating"], p["verified_rating"])
 	for c in comments:
-		add_comment(c["author"], c["picture"], c["comment"], c["date"])
+		add_comment(c["author"], c["picauthor"], c["comment"], c["date"])
 	
 	
 def add_user(id, fname, lname, date, isstaff):
-	u = User.objects.get_or_create(email=id)
+	u = User.objects.get_or_create(email=id)[0]
 	u.first_name = fname
 	u.last_name = lname
 	u.date_joined = date
@@ -114,21 +116,21 @@ def add_user(id, fname, lname, date, isstaff):
 	return u
 	
 def add_picture(author, picture, desc, date):
-	p = Picture.objects.get_or_create(author = author, picture = picture)
+	p = Picture.objects.get_or_create(author = User.objects.get(email=author), picture = picture)[0]
 	p.description = desc
 	p.date_published = date
 	p.save()
 	return p
 	
-def add_comment(author, picture, comment, date):
-	c = Comment.object.get_or_create(author = author, picture = picture)
+def add_comment(author, picauthor, comment, date):
+	c = Comment.objects.get_or_create(author = User.objects.get(email=author), picture = Picture.objects.filter(author=User.objects.get(email=picauthor)).first())[0]
 	c.comment = comment
 	c.date_published = date
 	c.save()
 	return c
 
-def add_rating(author, picture, true, false, verif):
-        r = Rating.object.get_or_create(author = author, picture = picture)
+def add_rating(author, true, false, verif):
+        r = Rating.objects.get_or_create(author = User.objects.get(email=author), picture = Picture.objects.filter(author=User.objects.get(email=author)).first())[0]
         r.truth_rating = true
         r.fake_rating = false
         r.verified_rating = verif
